0cce5b1010dec50c602978c45f5ed28a251f3501
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-06-02 03:42:01 +0300

    [rpc sockets] impl backend for room generation

--- pages/api/rpc/index.tsx
+++ pages/api/rpc/index.tsx
@@ -1,24 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { createRpcRoom } from 'api/server/controller';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { createRpcRoom, getNonPrivateRpcRooms } from 'api/server/controller';
+import { NextApiRequest } from 'next';
+import { Server } from 'socket.io';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  switch (req.method) {
-    case 'POST': {
-      const { owner } = req.body;
-      const r = await createRpcRoom(true, owner);
-      if (!r.error) {
-        return res.status(200).json(r);
-      }
-      return res.status(400).json(r.error);
-    }
-    case 'GET': {
-      console.log('get room');
-      break;
-    }
-    default:
+const SocketHandler = (req: NextApiRequest, res: any) => {
+  let io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
+  if (res.socket!.server.io) {
+    // Socket is already running
+  } else {
+    // Initialize socket
+    io = new Server(res.socket.server);
+    io.on('connect', async (socket) => {
+      let rooms = await getNonPrivateRpcRooms();
+      // get rooms on init
+      io.emit('get-rooms', { rooms });
+      // update rooms list on creation event
+      socket.on('create-room', async (data) => {
+        const { access, owner } = data;
+        const r = await createRpcRoom(access, owner);
+        if (!r.error && access) {
+          rooms = await getNonPrivateRpcRooms();
+          // get updated rooms
+          io.emit('get-rooms', { rooms });
+        }
+      });
+    });
+    res.socket.server.io = io;
   }
-  return res.status(400).json('Smth went wrong');
-}
 
-export default handler;
+  res.end();
+};
+
+export default SocketHandler;

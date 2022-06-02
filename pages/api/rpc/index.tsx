ed9509e82a32f3c99b73a8279eab2a62f1f0b217
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-06-02 22:11:00 +0300

    [rpc sockets] Socket: impl upd rooms with EventEmitter; impl events: join-room, leave-room, delete-room; upd events: create-room

--- pages/api/rpc/index.tsx
+++ pages/api/rpc/index.tsx
@@ -1,9 +1,18 @@
-import { createRpcRoom, getNonPrivateRpcRooms } from 'api/server/controller';
-import { NextApiRequest } from 'next';
+import {
+  addRoomMember,
+  createRpcRoom,
+  deleteRpcRoom,
+  removeRoomMember,
+  serverEventEmitter as server,
+} from 'api/server/controller';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { Server } from 'socket.io';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+import { getAndEmit } from 'util/helpers';
 
-const SocketHandler = (req: NextApiRequest, res: any) => {
+// todo: rewrite getAndEmit except interval deletion
+const SocketHandler = (req: NextApiRequest, res: NextApiResponse & { socket: any }) => {
+  // type of socket ? (not Socket)
   let io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
   if (res.socket!.server.io) {
     // Socket is already running
@@ -11,18 +20,35 @@ const SocketHandler = (req: NextApiRequest, res: any) => {
     // Initialize socket
     io = new Server(res.socket.server);
     io.on('connect', async (socket) => {
-      let rooms = await getNonPrivateRpcRooms();
-      // get rooms on init
-      io.emit('get-rooms', { rooms });
-      // update rooms list on creation event
+      await getAndEmit(io);
+      // listen to server interval deletion event to upd rooms for connected users
+      server.on('invterval-rooms-deleted', async () => {
+        await getAndEmit(io);
+      });
+      // update rooms members on joining/leaving
       socket.on('create-room', async (data) => {
         const { access, owner } = data;
         const r = await createRpcRoom(access, owner);
         if (!r.error && access) {
-          rooms = await getNonPrivateRpcRooms();
-          // get updated rooms
-          io.emit('get-rooms', { rooms });
+          await getAndEmit(io);
         }
+      });
+      // update rooms list on creation event
+      socket.on('join-room', async (data) => {
+        const { rid, uid } = data;
+        await addRoomMember(uid, rid);
+        await getAndEmit(io);
+      });
+      socket.on('leave-room', async (data) => {
+        const { rid, uid } = data;
+        await removeRoomMember(uid, rid);
+        await getAndEmit(io);
+      });
+      // update rooms list on deletion event
+      socket.on('delete-room', async (data) => {
+        const { rid } = data;
+        await deleteRpcRoom(rid);
+        await getAndEmit(io);
       });
     });
     res.socket.server.io = io;

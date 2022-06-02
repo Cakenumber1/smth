5b9656a9584c3cf013648746b683be49fe2cd80b
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-06-02 03:42:36 +0300

    [rpc sockets] impl fronted for room generation

--- pages/rpc/index.tsx
+++ pages/rpc/index.tsx
@@ -1,19 +1,58 @@
+import { DefaultEventsMap } from '@socket.io/component-emitter';
 import { useAuth } from 'AuthContext/';
-import axios from 'axios';
-import withPrivateRoom from 'components/HOCs/withPrivateRoomHOC';
+import withAuth from 'components/HOCs/withAuthHOC';
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import {
+  Dispatch, SetStateAction,
+  useEffect, useState,
+} from 'react';
+import io, { Socket } from 'socket.io-client';
+import { IRoomExt } from 'util/interfaces';
 
-const handleCreate = async (uid: string) => {
-  const res = await axios.post('/api/rpc/', { owner: uid });
-  console.log(res);
+// Func to initialize bridge
+const socketInitializer = async (
+  setSocket: Dispatch<SetStateAction<Socket<DefaultEventsMap, DefaultEventsMap> | null>>,
+) => {
+  await fetch('/api/rpc');
+  setSocket(io());
+};
+
+// Func to send event of 'create-room' to io listener
+const handleCreate = async (uid: string, socket: Socket<DefaultEventsMap, DefaultEventsMap>) => {
+  socket.emit('create-room', { owner: uid, access: true });
 };
 
 const Rpc: NextPage = () => {
   const { currentUser } = useAuth();
+  const [socket, setSocket] = useState<Socket<DefaultEventsMap, DefaultEventsMap> | null>(null);
+  const [data, setData] = useState<IRoomExt[]>();
+
+  // Call Initialization func once
+  useEffect(() => {
+    socketInitializer(setSocket);
+  }, []);
+
+  // Each frame check for subscribed events to happen & upd Data
+  useEffect(() => {
+    if (socket) {
+      socket.on('get-rooms', (smth) => {
+        setData(smth.rooms as IRoomExt[]);
+      });
+    }
+  });
+  // todo: split
   return (
     <div>
-      <button type="button" onClick={() => handleCreate(currentUser!._id)}>createroom</button>
+      <button type="button" onClick={() => handleCreate(currentUser!._id, socket!)}>create room</button>
+      {!data
+        ? <div>loading</div>
+        : (data && data.length)
+          ? (
+            <div>{data.map((room) => (
+              <div key={room._id}>{room._id}</div>))}
+            </div>
+          ) : <div>No rooms available</div>}
       <Link href="/">
         Home
       </Link>
@@ -21,4 +60,4 @@ const Rpc: NextPage = () => {
   );
 };
 
-export default withPrivateRoom(Rpc);
+export default withAuth(Rpc);
